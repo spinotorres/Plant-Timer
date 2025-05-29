1dfacc18e30039f729e68b611c42c6639a44fad9
Pedro Spinola Torres <97169260+spinotorres@users.noreply.github.com>
2025-05-29 16:19:28 -0300

    function title()

--- src/render.js
+++ src/render.js
@@ -1,6 +1,6 @@
 
 function button_info() {
-  const div = document.getElementsByClassName('text-info');
+  const div = document.querySelector('.text-info');
 
   if (div.style.display === 'none' || div.style.display === '') {
     div.innerText = "Esse aplicativo é destinado a lembrar o usuario de sempre regar suas plantas!";
@@ -8,4 +8,8 @@ function button_info() {
   } else {
     div.style.display = 'none'; // oculta a div
   }
+}
+
+function title() {
+  window.location.href = "main.html";
 }
