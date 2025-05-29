1f5bc2d3a2f660f4c8719d42d356ff2573d180df
Pedro Spinola Torres <97169260+spinotorres@users.noreply.github.com>
2025-05-29 14:03:25 -0300

    update assets and first codes

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 528,
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js'),
